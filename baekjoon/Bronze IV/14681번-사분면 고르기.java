55ab1a6ce98d94f39bfa39bdc2ea7cf7fc1aad3f
INT31302 <tkdwo287@gmail.com>
2020-03-18 20:31:09 +0900

    add complete algorithm

---
+++ baekjoon/Bronze IV/14681번-사분면 고르기.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        sc.close();
+        if (x > 0) {
+            if (y > 0)
+                System.out.println(1);
+            else
+                System.out.println(4);
+        } else {
+            if (y > 0)
+                System.out.println(2);
+            else
+                System.out.println(3);
+        }
+    }
+}
